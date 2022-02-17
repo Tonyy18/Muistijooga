120c3161789561606e1b65c3122e44625507e99a
Toni Isotalo <toni.isotalo@profitsoftware.com>
2022-02-17 11:37:27 +0200

    Added two more inputs

--- Arduino/mega/mega.ino
+++ Arduino/mega/mega.ino
@@ -1,8 +1,8 @@
 
 int analogTrigger = 1023 / 3;
-int pinCount = 12;
-int inputs[12] = {A0,A1,A2,A3,A4,A5,A6,A7,A8,A9,A10,A11};
-int outputs[12] = {2,3,4,5,6,7,8,9,10,11,12,13};
+int pinCount = 14;
+int inputs[14] = {A0,A1,A2,A3,A4,A5,A6,A7,A8,A9,A10,A11,A12,A13};
+int outputs[14] = {0,1,2,3,4,5,6,7,8,9,10,11,12,13};
 void setup() {
   // put your setup code here, to run once:
   for(int a = 0; a < pinCount; a++) {
--- Arduino/nano/nano.ino
+++ Arduino/nano/nano.ino
@@ -4,9 +4,9 @@ BLEService greetingService("180C");
 
 BLEStringCharacteristic greetingCharacteristic("2A56", BLERead | BLENotify, 13);
 
-int pinCount = 20;
+int pinCount = 22;
 int analogTrigger = 1023 / 3;
-int inputPins[20] =  {A0,A1,A2,A3,A4,A5,A6,A7,2,3,4,5,6,7,8,9,10,11,12,13};
+int inputPins[22] =  {A0,A1,A2,A3,A4,A5,A6,A7,0,1,2,3,4,5,6,7,8,9,10,11,12,13};
 
 void setup() {
   Serial.begin(9600);
